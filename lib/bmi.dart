01ae8da365f86af955251848299925b318c39d38
Manpriya Laksahan <manpriyapathirana@gmail.com>
2026-01-26 20:30:16 +0800

    UI done

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -1,3 +1,4 @@
+import 'package:bmi/constant.dart';
 import 'package:flutter/material.dart';
 
 class BMIcal extends StatefulWidget {
@@ -12,15 +13,144 @@ class _BMIcalState extends State<BMIcal> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('BMI Calculator'),
+        backgroundColor: Kbgcolor,
+        foregroundColor: kfgcolor,
+        title: Center(child: const Text('BMI Calculator')),
       ),
 
-      body: Column(
+      backgroundColor: Kbgcolor,
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: double.infinity,
 
-        children: [
-          const Text("Welcome to the BMI Calculator!"),
-          ElevatedButton(onPressed:() {}, child: Text("calculate BMI"))
-        ],
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+
+          children: [
+             Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                   
+                    Expanded(
+                      flex: 10,
+                      child: Container(
+                        decoration: kboxdecoration,
+                        padding: EdgeInsets.all(20),
+                        child:const Column(
+                          children: [
+                            Icon(Icons.male, size: 50, color: sTextcolor,), Text("Male", style: TextStyle(fontSize: 40, color: sTextcolor),)
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20,),
+                    Expanded(
+                      flex: 10,
+                      child: Container(
+                        decoration: kboxdecoration,
+                        padding: EdgeInsets.all(20),
+                        child: const Column(
+                          children: [
+                            Icon(Icons.female, size: 50,color: sTextcolor,), Text("Female", style: TextStyle(fontSize: 40, color: sTextcolor),)
+                          ],
+                        ),
+                      ),
+                    )
+                    
+                  
+                  ],
+             
+                  
+                ),
+
+                const SizedBox(height: 50,),
+
+
+                Container(
+               decoration: kboxdecoration,
+                 padding: EdgeInsets.all(20),
+
+                  child: Column(
+                    children: [
+                      Text("height" , style: TextStyle(color: sTextcolor,fontSize: 30),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text( "187 ",style: TextStyle(fontSize: 40 ,fontWeight: FontWeight.bold,color: sTextcolor),),
+                          Text(" cm", style: TextStyle(color: sTextcolor,fontSize: 30),),
+                  
+                        ],
+                      ),
+                      Slider(min:100.0,max:200.0, value: 187.0 , onChanged: (value){}),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 50,),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: kboxdecoration,
+                      padding: EdgeInsets.all(20),
+                      
+                        child: Column(
+                          children: [
+                            Text("weight" , style: TextStyle(fontSize: 20,color: sTextcolor)),
+                            Text(" 80 " , style: TextStyle(fontSize: 40 , fontWeight: FontWeight.bold,color: sTextcolor)),
+                            Row (
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              ElevatedButton(onPressed: (){}, child: Text("-")),
+                              ElevatedButton(onPressed: (){}, child: Text("+")),
+                        
+                        
+                            ],)
+                        
+                            
+                          ],
+                          
+                        ),
+                      ),
+                    ),
+                     const SizedBox(width: 20,),
+                    Expanded(
+                      child: Container(
+                      decoration: kboxdecoration,
+                      padding: EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Text("Age" ,style: TextStyle(fontSize: 20,color: sTextcolor)),
+                            Text(" 24 " ,  style: TextStyle(fontSize: 40 , fontWeight: FontWeight.bold,color: sTextcolor)),
+                            Row (
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                              ElevatedButton(onPressed: (){}, child: Icon(  Icons.remove)),
+                              ElevatedButton(onPressed: (){}, child: Icon(Icons.add)),
+                        
+                        
+                            ],)
+                        
+                            
+                          ],
+                          
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+
+              ],
+                         ),
+              SizedBox(height: 50,),
+            ElevatedButton(onPressed:() {}, child: Text("calculate BMI"))
+          ],
+        ),
       ),  
     );
   }
---
+++ lib/constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+  const Kbgcolor = Color(0xaa0A0E10);
+  const kfgcolor = Color(0xff8D8E98);
+
+  const KTileColor = Color.fromARGB(255, 28, 29, 59);
+  const sTextcolor = Color(0xffffffff);
+  const bTextcolor = Color(0xff8D8E98);
+
+  var kboxdecoration = BoxDecoration(borderRadius:BorderRadius.circular(10),color:KTileColor,);
+
+
+  
+
